0deb6d4d56293fbdb077bc6cb49865865a9064e0
Jonathan Gross <jonatha.gross@biodata.com>
2022-03-30 01:06:41 +0300

    adding mt.rb

---
+++ mt.rb
@@ -0,0 +1,31 @@
+host = ARGV[0].gsub("\n","")     # The web server
+port = ARGV[1].gsub("\n","")     # The web port 8080                          # Default HTTP port
+cmd  = ARGV[2].gsub("\n","")     # cmd "S for scale" "C1 for calibraiton"
+expected_number_of_responses = ARGV[3].gsub("\n","") || 1
+max_wait_time = 1
+if ARGV[4]
+  max_wait_time = ARGV[4].gsub("\n","")
+end
+# This is the HTTP request we send to fetch a file
+socket = TCPSocket.open(host,port)  # Connect to server
+last_read = []
+mutex = Mutex.new
+Thread.new do |t|
+  mutex.synchronize do
+    while line = socket.gets # Here we are reading line coming from the socket
+        last_read << line.chop
+    end
+  end
+end
+socket.puts("#{cmd}\r\n")
+i = 0
+
+while last_read.length < expected_number_of_responses.to_i
+ sleep 1
+ i += 1
+ if i > max_wait_time.to_i
+  break
+ end
+end
+
+puts last_read

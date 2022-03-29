63dd3de880e16dac5935a3dcb8b45e51dde90b1b
Jonathan Gross <jonatha.gross@biodata.com>
2022-03-29 23:53:54 +0300

    fixing pubnub clients name

--- pubnub_client.rb
+++ pubnub_client.rb
@@ -1,10 +1,10 @@
 require 'pubnub'
 
 lines = File.readlines("comm.dat")
-PUBLISH_KEY=lines[0]
-SUBSCRIBE_KEY=lines[1]
-FLOW_CREDS=lines[2]
-
+PUBLISH_KEY=lines[0].gsub("\n","")
+SUBSCRIBE_KEY=lines[1].gsub("\n","")
+FLOW_CREDS=lines[2].gsub("\n","")
+puts PUBLISH_KEY
 CHANNEL = "scales"
 commands = {get_weight: "get_weight", 
             reboot: "reboot",
@@ -16,12 +16,12 @@ def simulate
     result = rand(100)
     return_message = {"value": "#{result} gr"}
     reply = return_message.merge(FLOW_CREDS)
-    pubnub.publish(channel: CHANNEL, message: reply) do |env|
+    @pubnub.publish(channel: CHANNEL, message: reply) do |env|
        puts env.status
     end
 end 
 
-pubnub = Pubnub.new(publish_key: PUBLISH_KEY,
+@pubnub = Pubnub.new(publish_key: PUBLISH_KEY,
    subscribe_key: SUBSCRIBE_KEY,
    ssl: true,
    max_retries: 1,
@@ -38,11 +38,11 @@ callback = Pubnub::SubscribeCallback.new(
   }
 )
 
-pubnub.add_listener(callback: callback)
+@pubnub.add_listener(callback: callback)
 
-pubnub.subscribe( channels:[CHANNEL],  with_presence: true)
+@pubnub.subscribe( channels:[CHANNEL],  with_presence: true)
 
-pubnub.publish(channel: CHANNEL, message: {text: 'hello world'}) do |env|
+@pubnub.publish(channel: CHANNEL, message: {text: 'hello world'}) do |env|
   puts env.status
 end
 

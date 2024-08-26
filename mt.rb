4ac5f6207107acb5a9e4b63752d4744b6bb74a76
noa-kaplan <85630349+noa-kaplan@users.noreply.github.com>
2024-08-26 18:14:46 +0300

    Update mt.rb

--- mt.rb
+++ mt.rb
@@ -1,4 +1,42 @@
-puts "S  1.5 G"
+
+
+socket = TCPSocket.open(host, port)  # Connect to server
+last_read = []
+mutex = Mutex.new
+
+begin
+  reader_thread = Thread.new do
+    while line = socket.gets # Here we are reading line coming from the socket
+      mutex.synchronize do
+        last_read << line.chop
+      end
+    end
+  end
+
+  socket.puts("#{cmd}\r\n")
+
+  # Wait for the reader thread to finish or timeout
+  Timeout.timeout(max_wait_time.to_i) do
+    reader_thread.join
+  end
+rescue Timeout::Error
+  # Timeout occurred, move on
+ensure
+  socket.close unless socket.closed?
+  reader_thread.kill if reader_thread.alive?
+end
+
+puts last_read.join("\n")
+
+
+
+
+
+
+
+
+
+
 # require 'socket'
 # host = ARGV[0].gsub("\n","")     # The web server
 # port = ARGV[1].gsub("\n","")     # The web port 8080                          # Default HTTP port

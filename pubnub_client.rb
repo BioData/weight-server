3e69bbba8e6751f3dac48cbff240b9af413d4b6b
Noa <noa.kaplan@biodata.com>
2024-09-08 19:07:23 +0300

    Update SocketCommandExecutor

--- pubnub_client.rb
+++ pubnub_client.rb
@@ -2,62 +2,64 @@ require 'pubnub'
 require 'net/ping'
 require 'rest-client'
 require 'socket'
-require 'open3'
 
 class SocketCommandExecutor
-   def initialize(host, port, cmd, expected_number_of_responses = 1, max_wait_time = 1, pubnub)
-      @host = host.strip
-      @port = port.strip
-      @cmd = cmd.strip
-      @expected_number_of_responses = expected_number_of_responses.to_i
-      @max_wait_time = max_wait_time.to_i
-      @pubnub = pubnub
-   end
+  def initialize(host, port, cmd, expected_number_of_responses = 1, max_wait_time = 1, pubnub)
+    @host = host.strip
+    @port = port.strip
+    @cmd = cmd.strip
+    @expected_number_of_responses = expected_number_of_responses.to_i
+    @max_wait_time = max_wait_time.to_i
+    @pubnub = pubnub
+  end
 
-   def execute
-      command = build_command
-      puts("command #{command}")
-      last_read = []
+  def execute
+    socket = TCPSocket.open(@host, @port) # Connect to server
+    puts "Connected to #{@host} on port #{@port}"
+    last_read = []
+    mutex = Mutex.new
 
-      Open3.popen3(command) do |stdin, stdout, stderr, wait_thr|
-         stdin.puts @cmd  # Send the command to the server
-         stdin.close      # Close stdin when done sending
-
-         stdout_str = stdout.read
-         stderr_str = stderr.read
-
-         if wait_thr.value.success?
-            last_read = stdout_str.lines.map(&:chomp)
-         else
-            puts "Error: #{stderr_str.inspect}"
-            publish_message(stderr_str.inspect)
-         end
+    begin
+      reader_thread = Thread.new do
+        while line = socket.gets # Here we are reading line coming from the socket
+          puts "Reading from socket: #{line}"
+          mutex.synchronize do
+            last_read << line.chop
+          end
+        end
       end
 
-      sleep @max_wait_time
-      last_read.join("\n")
-   rescue => e
-      error_msg = "An error occurred: #{e.message}"
+      puts "Sending command: #{@cmd}"
+      socket.puts("#{@cmd}\r\n")
+
+      # Wait for the reader thread to finish or timeout
+      Timeout.timeout(@max_wait_time.to_i) do
+        reader_thread.join
+      end
+
+    rescue StandardError => e
+      error_msg = "SocketCommandExecutor error: Failed to execute command '#{@cmd}' on #{@host}:#{@port}. Error: #{e.message}"
       puts error_msg
-      publish_message(error_msg)
-   end
+      publish_message("#{LGPINUM}: #{error_msg}")
+    ensure
+      reader_thread.kill if reader_thread && reader_thread.alive?
+      socket.close if socket && !socket.closed?
+      puts 'Socket closed'
+    end
 
-   private
-
-   def build_command
-      "nc #{@host} #{@port}"
-   end
+    last_read.join("\n")
+  end
 end
 
 # Read configuration from file
-lines = File.readlines("comm.dat").map(&:chomp)
-PUBLISH_KEY=lines[0]
-SUBSCRIBE_KEY=lines[1]
-FLOW_CREDS=lines[2]
-SERVER=lines[3]
-LGPINUM=lines[4]
+lines = File.readlines('comm.dat').map(&:chomp)
+PUBLISH_KEY = lines[0]
+SUBSCRIBE_KEY = lines[1]
+FLOW_CREDS = lines[2]
+SERVER = lines[3]
+LGPINUM = lines[4]
 puts PUBLISH_KEY
-CHANNEL = "scales"
+CHANNEL = 'scales'
 # commands = {get_weight: "get_weight",
 #             reboot: "reboot",
 #             update: "update",
@@ -65,173 +67,172 @@ CHANNEL = "scales"
 
 # PubNub setup
 @pubnub = Pubnub.new(
-   publish_key: PUBLISH_KEY,
-   subscribe_key: SUBSCRIBE_KEY,
-   ssl: true,
-   max_retries: 1,
-   uuid: 'pace1'
- )
+  publish_key: PUBLISH_KEY,
+  subscribe_key: SUBSCRIBE_KEY,
+  ssl: true,
+  max_retries: 1,
+  uuid: 'pace1'
+)
 
 def publish_message(message)
-   @pubnub.publish(channel: CHANNEL, message: message) do |env|
-      puts env.status
-   end
+  @pubnub.publish(channel: CHANNEL, message: message) do |env|
+    puts env.status
+  end
 end
 
 def post_to_server(data)
-   response = RestClient.post(SERVER, data)
-   puts response
-rescue => e
-   error_msg = "Failed to post data to Labguru: #{e.message}"
-   puts error_msg
-   publish_message("#{LGPINUM}: #{error_msg}")
-
+  response = RestClient.post(SERVER, data)
+  puts "response: #{response}"
+rescue StandardError => e
+  error_msg = "Failed to post data to Labguru: #{e.message}"
+  puts error_msg
+  publish_message("#{LGPINUM}: #{error_msg}")
 end
 
-def simulate(params)
-   puts "simulate"
-   result = rand(100)
-   return_message = { value: "#{result} gr" }
-   publish_message("#{LGPINUM}: #{return_message}")
-   post_to_server(item: return_message)
+def simulate(_params)
+  puts 'simulate'
+  result = rand(100)
+  return_message = { value: "#{result} gr" }
+  publish_message("#{LGPINUM}: #{return_message}")
+  post_to_server(item: return_message)
 end
 
-def get_device_ip(params)
-   ip = Socket.ip_address_list.detect(&:ipv4_private?)&.ip_address
-   ip ||= 'Could not find IP'
-   publish_message("#{LGPINUM}: #{ip}")
+def get_device_ip(_params)
+  ip = Socket.ip_address_list.detect(&:ipv4_private?)&.ip_address
+  ip ||= 'Could not find IP'
+  publish_message("#{LGPINUM}: #{ip}")
 end
 
 def get_weight(params)
-   ip, port = params.values_at("ip", "port")
-   value = execute_socket_command(ip, port, 'S', '1', '5')
-   publish_message("#{LGPINUM}: #{value}")
-   post_to_server(item: { value: value, ip: ip, port: port })
+  ip, port = params.values_at('ip', 'port')
+  value = execute_socket_command(ip, port, 'S', '1', '5')
+  puts "value from balance = #{value}"
+  publish_message("#{LGPINUM}: #{value}")
+  post_to_server(item: { value: value, ip: ip, port: port })
 end
 
 def get_weight_with_fallback(params)
-   value = get_weight(params)
-   if value.nil? || value.empty?
-     value = "Balance did not respond"
-     publish_message("#{LGPINUM}: #{value}")
-   end
+  value = get_weight(params)
+  return unless value.nil? || value.empty?
+
+  value = 'Balance did not respond'
+  publish_message("#{LGPINUM}: #{value}")
 end
 
 def ping(params)
-   png = Net::Ping::HTTP.new(params)
-   publish_message("#{LGPINUM}: #{png.ping?}")
-   puts ("ping #{params} #{png.ping?}")
+  png = Net::Ping::HTTP.new(params)
+  publish_message("#{LGPINUM}: #{png.ping?}")
+  puts("ping #{params} #{png.ping?}")
 end
 
 def ping_equipment(params)
-   ip, port = params.values_at("ip", "port")
-   value = execute_socket_command(ip, port, "TIM", '1', '5')
-   publish_message("#{LGPINUM}: #{value}")
+  ip, port = params.values_at('ip', 'port')
+  value = execute_socket_command(ip, port, 'TIM', '1', '5')
+  publish_message("#{LGPINUM}: #{value}")
 
-   if value.nil? || value.empty?
-     value = "Balance did not respond"
-     publish_message("#{LGPINUM}: #{value}")
-     post_to_server(item: { value: value })
-   end
+  return unless value.nil? || value.empty?
+
+  value = 'Balance did not respond'
+  publish_message("#{LGPINUM}: #{value}")
+  post_to_server(item: { value: value })
 end
 
-def echo(params)
-   publish_message("#{LGPINUM}: I'm Here")
+def echo(_params)
+  publish_message("#{LGPINUM}: I'm Here")
 end
 
-def get_datfile(params)
-   lines = File.read("comm.dat")
-   publish_message("#{LGPINUM}: #{lines}")
+def get_datfile(_params)
+  lines = File.read('comm.dat')
+  publish_message("#{LGPINUM}: #{lines}")
 end
 
 def update_params(params)
-   lines = File.readlines("comm.dat").map(&:chomp)
-   if params["LGPINUM"] == LGPINUM
-     lines[params["line"]] = params["value"]
-     begin
-       File.write("comm.dat", lines.join("\n"))
-       publish_message("#{LGPINUM}: Updated")
-     rescue => e
-       publish_message("#{LGPINUM}: Error updating file - #{e.message}")
-       puts "Failed to update file: #{e.message}"
-     end
-   end
+  lines = File.readlines('comm.dat').map(&:chomp)
+  return unless params['LGPINUM'] == LGPINUM
+
+  lines[params['line']] = params['value']
+  begin
+    File.write('comm.dat', lines.join("\n"))
+    publish_message("#{LGPINUM}: Updated")
+  rescue StandardError => e
+    publish_message("#{LGPINUM}: Error updating file - #{e.message}")
+    puts "Failed to update file: #{e.message}"
+  end
 end
 
 def healthcheck(params)
-   puts "healthcheck #{params}"
-   df= `df / -h`
-   df = df.gsub("Filesystem", "")
-   df = df.gsub("Size","")
-   df = df.gsub("Used Avail Use% Mounted on\n/dev/root        ","").gsub("/\n","")
-   temp = `vcgencmd measure_temp`
-   mem = `vcgencmd get_mem arm   `
-   publish_message("#{LGPINUM}: #{df} \n #{temp} \n #{mem}")
-   publish_message("#{LGPINUM}")
+  puts "healthcheck #{params}"
+  df = `df / -h`
+  df = df.gsub('Filesystem', '')
+  df = df.gsub('Size', '')
+  df = df.gsub("Used Avail Use% Mounted on\n/dev/root        ", '').gsub("/\n", '')
+  temp = `vcgencmd measure_temp`
+  mem = `vcgencmd get_mem arm   `
+  publish_message("#{LGPINUM}: #{df} \n #{temp} \n #{mem}")
+  publish_message("#{LGPINUM}")
 end
 
-def update_pi(params)
-   publish_message("#{LGPINUM}: updating")
-   `sudo apt-get update -y`
-   `sudo apt-get upgrade --fix-missing -y`
-   publish_message("#{LGPINUM}: updated")
+def update_pi(_params)
+  publish_message("#{LGPINUM}: updating")
+  `sudo apt-get update -y`
+  `sudo apt-get upgrade --fix-missing -y`
+  publish_message("#{LGPINUM}: updated")
 end
 
-def update_repo(params)
-   publish_message("#{LGPINUM}: updating repo")
-   value = `git pull origin`
-   publish_message("#{LGPINUM}: #{value}")
+def update_repo(_params)
+  publish_message("#{LGPINUM}: updating repo")
+  value = `git pull origin`
+  publish_message("#{LGPINUM}: #{value}")
 end
 
 def reboot(params)
-   puts "ping #{params}"
-   publish_message("#{LGPINUM}: rebooting")
-   sleep 2
-   `sudo reboot`
+  puts "ping #{params}"
+  publish_message("#{LGPINUM}: rebooting")
+  sleep 2
+  `sudo reboot`
 end
 
 def remote_cmd(params)
-   ip, port, cmd = params.values_at("ip", "port", "cmd")
-   value = execute_socket_command(ip, port, cmd, params['arg1'], params['arg2'])
-   publish_message("#{LGPINUM}: #{value}")
-   post_to_server(item: { value: value, ip: ip, port: port })
+  ip, port, cmd = params.values_at('ip', 'port', 'cmd')
+  value = execute_socket_command(ip, port, cmd, params['arg1'], params['arg2'])
+  publish_message("#{LGPINUM}: #{value}")
+  post_to_server(item: { value: value, ip: ip, port: port })
 end
 
-
 def execute_socket_command(ip, port, cmd, num_of_lines, time_to_wait)
-   executor = SocketCommandExecutor.new(ip, port, cmd, num_of_lines, time_to_wait, @pubnub)
-   executor.execute
+  executor = SocketCommandExecutor.new(ip, port, cmd, num_of_lines, time_to_wait, @pubnub)
+  executor.execute
 end
 
 callback = Pubnub::SubscribeCallback.new(
-   message: ->(envelope) {
-      begin
-         puts "Received message: #{envelope.result[:data]}"
-         message = envelope.result[:data][:message]
+  message: lambda { |envelope|
+    begin
+      puts "Received message: #{envelope.result[:data]}"
+      message = envelope.result[:data][:message]
 
-         if message.is_a?(Hash)
-            cmd = message['cmd']
-            params = message['params']
-            send(cmd.to_sym, params) if cmd
-         else
-            puts "********** Unexpected message format: #{message.class} - #{message}"
-         end
-      rescue => e
-         puts "********** Error processing message: #{e.message}"
+      if message.is_a?(Hash)
+        cmd = message['cmd']
+        params = message['params']
+        send(cmd.to_sym, params) if cmd
+      else
+        puts "********** Unexpected message format: #{message.class} - #{message.inspect}"
       end
-   }
+    rescue StandardError => e
+      puts "********** Error processing message: #{e.message} - #{message.inspect}"
+    end
+  }
 )
 
 @pubnub.add_listener(callback: callback)
-@pubnub.subscribe( channels:[CHANNEL],  with_presence: true)
+@pubnub.subscribe(channels: [CHANNEL], with_presence: true)
 
 publish_message("#{LGPINUM}: I'm up")
 
 time = Time.now
-while(true)
- #do nothing
- if Time.now > time + 60 * 5   #every 5 minutes
-   healthcheck({})
-   time = Time.now
- end
+while true
+  # do nothing
+  if Time.now > time + 60 * 5 # every 5 minutes
+     healthcheck({})
+    time = Time.now
+  end
 end
